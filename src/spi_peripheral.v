f7dbafb2b9aa24235ee285cdc1f39cc884acfa4c
elvis77-op <lbwelvisli@hotmail.com>
2025-10-03 01:17:31 +0000

    pass gds

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -2,23 +2,12 @@ module spi_peripheral (
     input  wire [7:0]  ui_in,
     input  wire       clk,  // system clock
     input wire       rst_n, // reset_n - low to reset
-    output  wire [7:0] en_reg_out_7_0, // Output register
-    output  wire [7:0] en_reg_out_15_8,
-    output  wire [7:0] en_reg_pwm_7_0,
-    output  wire [7:0] en_reg_pwm_15_8,
-    output  wire [7:0] pwm_duty_cycle
+    output  reg [7:0] en_reg_out_7_0, // Output register
+    output  reg [7:0] en_reg_out_15_8,
+    output  reg [7:0] en_reg_pwm_7_0,
+    output  reg [7:0] en_reg_pwm_15_8,
+    output  reg [7:0] pwm_duty_cycle
 );
-    reg [7:0] en_reg_out_7_0_r;
-    assign en_reg_out_7_0 = en_reg_out_7_0_r;
-    reg [7:0] en_reg_out_15_8_r;
-    assign en_reg_out_15_8 = en_reg_out_15_8_r;
-    reg [7:0] en_reg_pwm_7_0_r;
-    assign en_reg_pwm_7_0 = en_reg_pwm_7_0_r;
-    reg [7:0] en_reg_pwm_15_8_r;
-    assign en_reg_pwm_15_8 = en_reg_pwm_15_8_r;
-    reg [7:0] pwm_duty_cycle_r;
-    assign pwm_duty_cycle = pwm_duty_cycle_r;
-
     wire sclk = ui_in[0];
     wire ncs  = ui_in[2];
     wire copi = ui_in[1];
@@ -26,19 +15,14 @@ module spi_peripheral (
     reg [15:0] buffer;
     reg [4:0] bit_counter; 
 
-    reg sclk_dly1, sclk_dly2, ncs_sync1, ncs_sync2;
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            sclk_dly1 <= 1'b0;
-            sclk_dly2 <= 1'b0;
-            ncs_sync1 <= 1'b1; 
-            ncs_sync2 <= 1'b1;
-        end else begin
-            sclk_dly1 <= sclk;
-            sclk_dly2 <= sclk_dly1;
-            ncs_sync1 <= ncs;
-            ncs_sync2 <= ncs_sync1;
-        end
+    reg sclk_dly1, sclk_dly2, ncs_sync1, ncs_sync2, copi_sync1, copi_sync2;
+    always @(posedge clk) begin
+        sclk_dly1 <= sclk;
+        sclk_dly2 <= sclk_dly1;
+        copi_sync1 <= copi;
+        ncs_sync1 <= ncs;
+        ncs_sync2 <= ncs_sync1;
+        copi_sync2 <= copi_sync1;
     end
 
     wire sclk_posedge = (~sclk_dly2) & sclk_dly1;
@@ -52,7 +36,7 @@ module spi_peripheral (
             transaction_ready <= 1'b0;
         end else if (ncs_sync2 == 1'b0) begin
         end else begin
-            if (ncs_posedge) begin
+            if (ncs_negedge) begin
                 transaction_ready <= 1'b1;
             end else if (transaction_processed) begin
                 transaction_ready <= 1'b0;
@@ -64,38 +48,40 @@ module spi_peripheral (
     
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            en_reg_out_7_0_r <= 8'h00;
-            en_reg_out_15_8_r <= 8'h00;
-            en_reg_pwm_7_0_r <= 8'h00;
-            en_reg_pwm_15_8_r <= 8'h00;
-            pwm_duty_cycle_r <= 8'h00;
+            en_reg_out_7_0 <= 8'h00;
+            en_reg_out_15_8 <= 8'h00;
+            en_reg_pwm_7_0 <= 8'h00;
+            en_reg_pwm_15_8 <= 8'h00;
+            pwm_duty_cycle <= 8'h00;
             buffer <= 16'd0;
             bit_counter <= 5'd0;
-        end else begin
-            if (!ncs) begin
+            transaction_processed <= 1'b0;
+        end else if(transaction_ready && !transaction_processed) begin
+            if (ncs_sync1 == 1'b0) begin
                 if (sclk_posedge) begin
-                    if(transaction_ready && !transaction_processed) begin 
-                        if (bit_counter == 5'd16 && buffer[15] == 1'b1) begin
-                            case (buffer[14:8])
-                                7'h00: en_reg_out_7_0_r <= buffer[7:0];
-                                7'h01: en_reg_out_15_8_r <= buffer[7:0];
-                                7'h02: en_reg_pwm_7_0_r <= buffer[7:0];
-                                7'h03: en_reg_pwm_15_8_r <= buffer[7:0];
-                                7'h04: pwm_duty_cycle_r <= buffer[7:0];
-                                default: ;
-                            endcase
-                            transaction_processed <= 1'b1;
-                        end else begin
-                            buffer <= {buffer[14:0], copi};
-                            bit_counter <= bit_counter + 5'd1;
-                        end
+                    if (bit_counter < 5'd16) begin
+                        buffer <= {buffer[14:0], copi_sync2};
+                        bit_counter <= bit_counter + 5'd1;
                     end
                 end
-            end else if (!transaction_ready && transaction_processed) begin
-                buffer <= 16'd0;
-                bit_counter <= 5'd0;
-                transaction_processed <= 1'b0;
             end
+            if (bit_counter == 5'd16 && buffer[15] == 1'b1) begin
+            $display("[%0t] SPI transaction complete: buffer = %b (addr = %h, data = %h)", 
+             $time, buffer, buffer[14:8], buffer[7:0]);
+                case (buffer[14:8])
+                    7'h00: en_reg_out_7_0 <= buffer[7:0];
+                    7'h01: en_reg_out_15_8 <= buffer[7:0];
+                    7'h02: en_reg_pwm_7_0 <= buffer[7:0];
+                    7'h03: en_reg_pwm_15_8 <= buffer[7:0];
+                    7'h04: pwm_duty_cycle <= buffer[7:0];
+                    default: ;
+                endcase
+                transaction_processed <= 1'b1;
+            end
+        end else if (!transaction_ready && transaction_processed) begin
+            buffer <= 16'd0;
+            bit_counter <= 5'd0;
+            transaction_processed <= 1'b0;
         end
     end
 
